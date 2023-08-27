dfe5cd789089c8d843700921be917357bb91f58f
fatesArbiter <minka.dmitry@yandex.ru>
2023-08-27 21:51:05 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.gallery"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.gallery"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = false
+        dataBinding = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/example/gallery/MainViewModel.kt
@@ -0,0 +1,175 @@
+package com.example.gallery
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.app.RecoverableSecurityException
+import android.content.ContentProvider
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.content.IntentSender
+import android.database.ContentObserver
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import android.os.Handler
+import android.provider.MediaStore
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+    private val _images = MutableLiveData<List<MediaStoreImage>>()
+    val images: LiveData<List<MediaStoreImage>> get() = _images
+
+    private var contentObserver: ContentObserver? = null
+
+    private var pendingDeleteImage: MediaStoreImage? = null
+    private val _permissionNeededForDelete = MutableLiveData<IntentSender?>()
+    val permissionNeededForDelete: LiveData<IntentSender?> = _permissionNeededForDelete
+
+    fun loadImages() {
+        viewModelScope.launch {
+            val imageList = queryImages()
+            _images.postValue(imageList)
+
+            if (contentObserver == null) {
+                contentObserver = getApplication<Application>().contentResolver.registerObserver(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                ) {
+                    loadImages()
+                }
+            }
+        }
+    }
+
+    fun deleteImage(image: MediaStoreImage) {
+        viewModelScope.launch {
+            performDeleteImage(image)
+        }
+    }
+
+    fun deletePendingImage() {
+        pendingDeleteImage?.let { image ->
+            pendingDeleteImage = null
+            deleteImage(image)
+        }
+    }
+
+    private suspend fun queryImages(): List<MediaStoreImage> {
+        val images = mutableListOf<MediaStoreImage>()
+
+        withContext(Dispatchers.IO) {
+            val projection = arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_ADDED
+            )
+            val selection = "${MediaStore.Images.Media.DATE_ADDED} >= ?"
+
+            val selectionArgs = arrayOf(
+                dateToTimestamp(day = 22, month = 10, year = 2008).toString()
+            )
+
+            val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
+
+            getApplication<Application>().contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                sortOrder
+            )?.use { cursor ->
+
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val dateModifiedColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_ADDED)
+                val displayNameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+
+                Log.i(TAG, "Found ${cursor.count} images")
+                while (cursor.moveToNext()) {
+
+                    val id = cursor.getLong(idColumn)
+                    val dateModified =
+                        Date(TimeUnit.SECONDS.toMillis(cursor.getLong(dateModifiedColumn)))
+                    val displayName = cursor.getString(displayNameColumn)
+
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        id
+                    )
+
+                    val image = MediaStoreImage(id, displayName, dateModified, contentUri)
+                    images += image
+                    Log.v(TAG, "Added image: $image")
+                }
+            }
+        }
+
+        Log.v(TAG, "Found ${images.size} images")
+        return images
+    }
+
+    private suspend fun performDeleteImage(image: MediaStoreImage) {
+        withContext(Dispatchers.IO) {
+            try {
+                getApplication<Application>().contentResolver.delete(
+                    image.contentUri,
+                    "${MediaStore.Images.Media._ID} = ?",
+                    arrayOf(image.id.toString())
+                )
+            } catch (securityException: SecurityException) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val recoverableSecurityException =
+                        securityException as? RecoverableSecurityException
+                            ?: throw securityException
+
+                    // Signal to the Activity that it needs to request permission and
+                    // try the delete again if it succeeds.
+                    pendingDeleteImage = image
+                    _permissionNeededForDelete.postValue(
+                        recoverableSecurityException.userAction.actionIntent.intentSender
+                    )
+                } else {
+                    throw securityException
+                }
+            }
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    @SuppressLint("SimpleDateFormat")
+    private fun dateToTimestamp(day: Int, month: Int, year: Int): Long =
+        SimpleDateFormat("dd.MM.yyyy").let { formatter ->
+            TimeUnit.MICROSECONDS.toSeconds(formatter.parse("$day.$month.$year")?.time ?: 0)
+        }
+
+    override fun onCleared() {
+        contentObserver?.let {
+            getApplication<Application>().contentResolver.unregisterContentObserver(it)
+        }
+    }
+}
+
+private fun ContentResolver.registerObserver(
+    uri: Uri,
+    observer: (selfChange: Boolean) -> Unit
+): ContentObserver {
+    val contentObserver = object : ContentObserver(Handler()) {
+        override fun onChange(selfChange: Boolean) {
+            observer(selfChange)
+        }
+    }
+    registerContentObserver(uri, true, contentObserver)
+    return contentObserver
+}
+
+private const val TAG = "MainVM"
